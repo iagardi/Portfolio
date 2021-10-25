09afe3ca1228141116979cf565d18ba307656d33
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-10-25 17:27:15 +0100

    Temporary fix, import the images from src/assets/imgs/fileName.png and use it in data file

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,31 @@
-const projects = [{
-   name: "Calculator",
-   description: "A simple calculator project, created in the n-th week of the _nology course",
-   technology: ["HTML", "Sass", "JavaScript", "Cypress"],
-   hostedURL: "https://iagardi.github.io/Calculator/",
-   githubURL: "https://github.com/iagardi/Calculator",
-   photo: "../../assets/images/calculator.png"
-}, {
-   name: "Morse translator",
-   description: "An English-to-morse and morse-to-English translator, created on the n-th week of the _nology course",
-   technology: ["HTML", "Sass", "JavaScript"],
-   hostedURL: "https://iagardi.github.io/morse-translator/",
-   githubURL: "https://github.com/iagardi/morse-translator",
-   photo: "../../assets/images/morse-translator.png"
-}, {
-   name: "Punk API",
-   description: "A small web app to browse and search the products of Brewdog, using the data from 'https://punkapi.com/'",
-   technology: ["React"],
-   hostedURL: "https://iagardi.github.io/Punk-API/",
-   githubURL: "https://github.com/iagardi/Punk-API",
-}]
+import calculator from "../assets/images/calculator.png";
+import morse from "../assets/images/morse-translator.png";
 
-export default projects
+const projects = [
+  {
+    name: "Calculator",
+    description: "A simple calculator project, created in the n-th week of the _nology course",
+    technology: ["HTML", "Sass", "JavaScript", "Cypress"],
+    hostedURL: "https://iagardi.github.io/Calculator/",
+    githubURL: "https://github.com/iagardi/Calculator",
+    photo: calculator,
+  },
+  {
+    name: "Morse translator",
+    description: "An English-to-morse and morse-to-English translator, created on the n-th week of the _nology course",
+    technology: ["HTML", "Sass", "JavaScript"],
+    hostedURL: "https://iagardi.github.io/morse-translator/",
+    githubURL: "https://github.com/iagardi/morse-translator",
+    photo: morse,
+  },
+  {
+    name: "Punk API",
+    description:
+      "A small web app to browse and search the products of Brewdog, using the data from 'https://punkapi.com/'",
+    technology: ["React"],
+    hostedURL: "https://iagardi.github.io/Punk-API/",
+    githubURL: "https://github.com/iagardi/Punk-API",
+  },
+];
+
+export default projects;
