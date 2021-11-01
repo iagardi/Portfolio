eff92525186e32825c059a1d7ba3d2458c5aa8e0
iagardi <garbigz@gmail.com>
2021-11-01 09:31:57 +0000

    added snake project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,7 @@
-import calculator from '../assets/images/calculator.png'
-import morseTranslator from '../assets/images/morse-translator.png'
+import calculatorPhoto from '../assets/images/calculator.png'
+import morseTranslatorPhoto from '../assets/images/morse-translator.png'
+import snakePhoto from '../assets/images/snake.png'
+import punkPhoto from '../assets/images/punk-api.png'
 
 const projects = [{
    name: "Calculator",
@@ -7,26 +9,28 @@ const projects = [{
    technology: ["html", "sass", "javascript", "cypress"],
    hostedURL: "https://iagardi.github.io/Calculator/",
    githubURL: "https://github.com/iagardi/Calculator",
-   photo: calculator
+   photo: calculatorPhoto
 }, {
    name: "Morse translator",
    description: "An English-to-morse and morse-to-English translator, created on the n-th week of the _nology course",
    technology: ["html", "sass", "javascript"],
    hostedURL: "https://iagardi.github.io/morse-translator/",
    githubURL: "https://github.com/iagardi/morse-translator",
-   photo: morseTranslator
+   photo: morseTranslatorPhoto
 }, {
    name: "Punk API",
    description: "A small web app to browse and search the products of Brewdog, using the data from 'https://punkapi.com/'",
    technology: ["react"],
    hostedURL: "https://iagardi.github.io/Punk-API/",
    githubURL: "https://github.com/iagardi/Punk-API",
+   photo: punkPhoto
 }, {
    name: "Snake",
    description: "My remake of Nokia's classic Snake game",
    technology: ["html", "sass", "javascript"],
    hostedURL: "https://iagardi.github.io/Snake/",
    githubURL: "https://github.com/iagardi/Snake",
+   photo: snakePhoto
 }]
 
 export default projects
